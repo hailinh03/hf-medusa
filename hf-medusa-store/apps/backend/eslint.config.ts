d3c4c52e9b35588dc120084c4bbc083feb04aa68
hailinh03 <hailinh2910203@gmail.com>
2026-07-10 18:32:28 +0700

    feat(backend): update ESLint configuration and pnpm workspace settings

--- hf-medusa-store/apps/backend/eslint.config.ts
+++ hf-medusa-store/apps/backend/eslint.config.ts
@@ -1,4 +1,12 @@
 import { defineConfig } from "eslint/config"
 import medusa from "@medusajs/eslint-plugin"
 
-export default defineConfig([...medusa.configs.recommended])
+export default defineConfig([
+  ...medusa.configs.recommended,
+  {
+    files: ["src/modules/**/models/**/*.{ts,js}"],
+    rules: {
+      "@medusajs/link-no-cross-module-relationship": "off",
+    },
+  },
+])
